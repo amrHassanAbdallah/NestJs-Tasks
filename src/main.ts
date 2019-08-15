5b057228f35e6cc3d151244553c2a4d2c2286f43
Amr Hassan <amrh434@gmail.com>
2019-08-15 15:55:57 +0200

    refactor: added swagger to dev.

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import * as config from 'config';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const serverConfig = config.get('server');
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: serverConfig.allowed_origins});
+  app.enableCors({ origin: serverConfig.allowed_origins });
   logger.log(`White listed origins ${JSON.stringify(serverConfig.allowed_origins)}`);
   const port = process.env.PORT || serverConfig.port;
+
+  if (process.env.NODE_ENV === 'development') {
+    const options = new DocumentBuilder()
+      .setTitle('Task Endpoints')
+      .setDescription('The cats API description')
+      .setVersion('1.0')
+      .addTag('tasks')
+      .build();
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('api', app, document);
+  }
+
+
   await app.listen(port);
   logger.log(`App listening on port ${port}`);
 }
